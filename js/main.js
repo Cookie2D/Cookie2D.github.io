91535aafffc574f38febc279a10a9365e1eb8bab
Cookie2D <46797011+Cookie2D@users.noreply.github.com>
2020-02-10 19:00:19 +0200

    Modal Fixed

--- js/main.js
+++ js/main.js
@@ -13,17 +13,30 @@ let mySwiper = new Swiper('.swiper-container', {
 
 });
 
-let body = document.querySelector('body');
-let menuButton = document.querySelector('.button_burger');
-let menu = document.querySelector('.header');
-let modalButton = document.querySelector('#modal-btn');
-let descriptionButton = document.querySelector('#description-btn')
-let modal = document.querySelector('.modal');
+const body = document.querySelector('body');
+      menu = document.querySelector('.header');
+      menuButton = document.querySelector('.button_burger');
+      modalButton = document.querySelector('#modal-btn');
+      descriptionButton = document.querySelector('#description-btn')
+      modal = document.querySelector('.modal');
+      modalDialog = document.querySelector('.modal__dialog')
 
 menuButton.addEventListener('click', function () {
+  event.stopPropagation();
   menu.classList.toggle('header--active');
-  this.classList.toggle('button_burger--active');
+  menuButton.classList.toggle('button_burger--active');
 });
+
+body.addEventListener('click', function () {
+  if (menu.classList.contains('header--active')) {
+    menu.classList.remove('header--active');
+    menuButton.classList.remove('button_burger--active');
+  }
+})
+
+menu.addEventListener('click', function () {
+  event.stopPropagation();
+})
 
 modalButton.addEventListener('click', function () {
   modal.classList.add('hide');
@@ -34,3 +47,12 @@ descriptionButton.addEventListener('click', function () {
   modal.classList.remove('hide');
   body.classList.add('no-scroll')
 });
+
+modal.addEventListener('click', function () {
+  modal.classList.add('hide');
+  body.classList.remove('no-scroll')
+})
+
+modalDialog.addEventListener('click', function () {
+  event.stopPropagation();
+})
