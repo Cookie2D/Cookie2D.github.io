2e397264c19857f5c4dcab72ed75951ce9a32ef6
Cookie2D <46797011+Cookie2D@users.noreply.github.com>
2020-02-11 00:17:24 +0200

    Modal Final Version

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let mySwiper = new Swiper('.swiper-container', {
+const mySwiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   grabCursor: true,
   loop: true,
@@ -14,12 +14,9 @@ let mySwiper = new Swiper('.swiper-container', {
 });
 
 const body = document.querySelector('body');
-      menu = document.querySelector('.header');
-      menuButton = document.querySelector('.button_burger');
-      modalButton = document.querySelector('#modal-btn');
-      descriptionButton = document.querySelector('#description-btn')
-      modal = document.querySelector('.modal');
-      modalDialog = document.querySelector('.modal__dialog')
+menu = document.querySelector('.header');
+menuButton = document.querySelector('.button_burger');
+descriptionButton = document.querySelector('#description-btn')
 
 menuButton.addEventListener('click', function () {
   event.stopPropagation();
@@ -38,21 +35,14 @@ menu.addEventListener('click', function () {
   event.stopPropagation();
 })
 
-modalButton.addEventListener('click', function () {
-  modal.classList.add('hide');
-  body.classList.remove('no-scroll')
-});
-
+// Description
 descriptionButton.addEventListener('click', function () {
-  modal.classList.remove('hide');
-  body.classList.add('no-scroll')
+  menu.classList.remove('header--active');
+  menuButton.classList.remove('button_burger--active');
+  
+  swal('Ведьмак', `«Ведьмак» (англ. The Witcher) — американо-польский фэнтезийный телесериал, снятый по мотивам одноименной серии романов Анджея Сапковского[1]. Премьера сериала состоялась 20 декабря 2019 года на стриминг-сервисе Netflix. 13 ноября 2019 года был официально анонсирован второй сезон.
+
+  Ведьмак Геральт из Ривии, мутант и убийца чудовищ, изо всех сил старается найти своё место в мире, где люди часто оказываются намного хуже монстров[2]. Сюжет первого сезона большей частью представляет собой вольную интерпретацию некоторых рассказов первых двух книг саги о Ведьмаке — «Последнее желание» и «Меч Предназначения».`, 'info', {
+    button: 'Закрыть'
+  });
 });
-
-modal.addEventListener('click', function () {
-  modal.classList.add('hide');
-  body.classList.remove('no-scroll')
-})
-
-modalDialog.addEventListener('click', function () {
-  event.stopPropagation();
-})
