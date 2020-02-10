7684398c75d32a66c2d9c22a8bf46f5bce954376
Cookie2D <46797011+Cookie2D@users.noreply.github.com>
2020-02-10 12:34:37 +0200

    addModal

---
+++ main.js
@@ -0,0 +1,33 @@
+let mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  navigation: {
+    nextEl: '.button_arrow',
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2
+    }
+  }
+
+});
+
+let menuButton = document.querySelector('.button_burger');
+let menu = document.querySelector('.header');
+let modalButton = document.querySelector('#modal-btn');
+let descriptionButton = document.querySelector('#description-btn')
+let modal = document.querySelector('.modal');
+
+menuButton.addEventListener('click', function () {
+  menu.classList.toggle('header--active');
+  this.classList.toggle('button_burger--active');
+});
+
+modalButton.addEventListener('click', function () {
+  modal.classList.add('hide');
+});
+
+descriptionButton.addEventListener('click', function () {
+  modal.classList.remove('hide');
+})
