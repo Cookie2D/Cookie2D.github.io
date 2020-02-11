e5f18bd081ad4ef433b63b04e24d484baec67d99
Cookie2D <46797011+Cookie2D@users.noreply.github.com>
2020-02-11 02:16:20 +0200

    JQuery reWrite

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+
+function toggleMenu() {
+  $('.header').toggleClass('show');
+  $('.button_burger').toggleClass('button_burger--active');
+}
+
+$('.button_burger').on('click', () => {
+  toggleMenu();
+  event.stopPropagation();
+});
+
+// Close by click on mask
+$('body').on('click', () => {
+  $('.header').on('click', () => event.stopPropagation());
+
+  if ($('.header').hasClass('show')) toggleMenu();
+})
+
+// Description modal window
+$('.description__button').on('click', () => {
+  toggleMenu();
+
+  //Alert modal window
+  swal('Ведьмак', `«Ведьмак» (англ. The Witcher) — американо-польский фэнтезийный телесериал, снятый по мотивам одноименной серии романов Анджея Сапковского[1]. Премьера сериала состоялась 20 декабря 2019 года на стриминг-сервисе Netflix. 13 ноября 2019 года был официально анонсирован второй сезон.
+  
+  Ведьмак Геральт из Ривии, мутант и убийца чудовищ, изо всех сил старается найти своё место в мире, где люди часто оказываются намного хуже монстров[2]. Сюжет первого сезона большей частью представляет собой вольную интерпретацию некоторых рассказов первых двух книг саги о Ведьмаке — «Последнее желание» и «Меч Предназначения».`, 'info', {
+    button: 'Закрыть'
+  });
+});
+
+
+//Slider Settings
 const mySwiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   grabCursor: true,
@@ -10,39 +42,4 @@ const mySwiper = new Swiper('.swiper-container', {
       slidesPerView: 2
     }
   }
-
-});
-
-const body = document.querySelector('body');
-menu = document.querySelector('.header');
-menuButton = document.querySelector('.button_burger');
-descriptionButton = document.querySelector('#description-btn')
-
-menuButton.addEventListener('click', function () {
-  event.stopPropagation();
-  menu.classList.toggle('header--active');
-  menuButton.classList.toggle('button_burger--active');
-});
-
-body.addEventListener('click', function () {
-  if (menu.classList.contains('header--active')) {
-    menu.classList.remove('header--active');
-    menuButton.classList.remove('button_burger--active');
-  }
-})
-
-menu.addEventListener('click', function () {
-  event.stopPropagation();
-})
-
-// Description
-descriptionButton.addEventListener('click', function () {
-  menu.classList.remove('header--active');
-  menuButton.classList.remove('button_burger--active');
-  
-  swal('Ведьмак', `«Ведьмак» (англ. The Witcher) — американо-польский фэнтезийный телесериал, снятый по мотивам одноименной серии романов Анджея Сапковского[1]. Премьера сериала состоялась 20 декабря 2019 года на стриминг-сервисе Netflix. 13 ноября 2019 года был официально анонсирован второй сезон.
-
-  Ведьмак Геральт из Ривии, мутант и убийца чудовищ, изо всех сил старается найти своё место в мире, где люди часто оказываются намного хуже монстров[2]. Сюжет первого сезона большей частью представляет собой вольную интерпретацию некоторых рассказов первых двух книг саги о Ведьмаке — «Последнее желание» и «Меч Предназначения».`, 'info', {
-    button: 'Закрыть'
-  });
 });
